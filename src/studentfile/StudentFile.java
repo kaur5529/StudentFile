3da5d1e36ebf71a9a7945d959277279c10c4804a
kaur5529 <kaur5529@sheridancollege.ca>
2019-05-21 13:11:46 -0400

    1st Commit

---
+++ src/studentfile/StudentFile.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentfile;
+
+/**
+ *
+ * @author grewal
+ */
+public class StudentFile {
+     public static void main(String[] args)
+   {
+       //declaring variables
+       int num1,num2,sum;
+       //Input-assigning values to variables
+       num1=5;
+       num2=9;
+       //processing
+       sum=num1+num2;
+       //output
+       System.out.println("Sum of "+num1+" and "+num2+
+               " is "+sum);
+
+   }
+}
